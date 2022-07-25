b25ff5d374632d5cd3f519a672f9c43ecc7fe603
Scott DeGraw <scott.degraw@sjc.ox.ac.uk>
2022-07-25 12:11:44 +0100

    Added simple library to read meta data files

---
+++ src/meta_data_reader/meta_data_reader.hpp
@@ -0,0 +1,9 @@
+#ifndef CPP_TEMPLATE_LPF_META_DATA_READER_HPP
+#define CPP_TEMPLATE_LPF_META_DATA_READER_HPP
+
+#include <map>
+#include <string>
+
+std::map<std::string, std::string> meta_data_reader(const std::string &filename);
+
+#endif// CPP_TEMPLATE_LPF_META_DATA_READER_HPP
