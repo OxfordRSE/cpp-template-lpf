22056efb12550b64df8ce27c28e85ff99617f203
Scott DeGraw <scott.degraw@sjc.ox.ac.uk>
2022-07-26 10:06:45 +0100

    Removed custom metadata reader in favour of json library

--- src/meta_data_reader/meta_data_reader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CPP_TEMPLATE_LPF_META_DATA_READER_HPP
-#define CPP_TEMPLATE_LPF_META_DATA_READER_HPP
-
-#include <map>
-#include <string>
-
-std::map<std::string, std::string> meta_data_reader(const std::string &filename);
-
-#endif// CPP_TEMPLATE_LPF_META_DATA_READER_HPP
