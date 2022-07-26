ae0f969aa56be526aadfb7e0b47ac5d47db08a13
Scott DeGraw <scott.degraw@sjc.ox.ac.uk>
2022-07-26 13:10:45 +0100

    Added error message if input data is not divisible by the block size

--- src/readers_and_writers/BlockReader.cpp
+++ src/readers_and_writers/BlockReader.cpp
@@ -13,15 +13,18 @@ BlockReader::BlockReader(const std::string &filename, std::size_t sizeOfBlock)
   const std::filesystem::path pathToData(filename);
   const std::size_t sizeInBytes = std::filesystem::file_size(pathToData);
   const std::size_t numOfData = sizeInBytes / sizeof(double);
-  nBlocks = std::ceil(numOfData / sizeOfBlock);
+  double nBlocksDouble(static_cast<double>(numOfData) / static_cast<double>(sizeOfBlock));
+  if (std::floor(nBlocksDouble) != std::ceil(nBlocksDouble)) {
+    throw std::system_error(errno, std::system_category(), "The input data is not divisible by the block size");
+  }
+  nBlocks = std::floor(nBlocksDouble);
 }
 
 std::size_t BlockReader::numOfBlocks() const { return nBlocks; }
 
 void BlockReader::readNextBlock(std::vector<double>::iterator start)
 {
-  //TODO maybe but size check on input block
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  //  NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   auto *const read_buffer = reinterpret_cast<char *>(&(*start));// cppcheck-suppress invalidPointerCast
   reader.read(read_buffer, static_cast<std::streamsize>(blockSize * sizeof(double)));
 }
